63a659d0fae35c0468984836fa4f63d4b64743ef
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-09-21 18:02:05 +0800

    feat: 🎸 clean code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "@ruguoapp/jk-theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
